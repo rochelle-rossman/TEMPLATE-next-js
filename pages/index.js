8ae4a91a1ebebeaf929c68e8bab128eb32d84d74
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-07-11 16:53:31 -0500

    completed code along in class

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
+import { useState } from 'react';
+
 function Home() {
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue((prevState) => prevState + 1);
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home! </h1>
-    </div>
+    <>
+      <h1>Counter Title</h1>
+      <h2>{value}</h2>
+      <button type="button" onClick={handleClick}>
+        Increment
+      </button>
+      <button type="button" onClick={() => setValue((prevState) => prevState - 1)}>
+        Decrement
+      </button>
+      <button type="button" onClick={() => setValue(0)}>
+        Reset
+      </button>
+    </>
   );
 }
 
---
+++ pages/rochelle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function rochelle() {
+  return (
+    <div>rochelle</div>
+  );
+}
